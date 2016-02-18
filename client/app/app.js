e2c41701cfb259de733dd2ee121900c6f1dbb578
Gar Lee <gardarr@gmail.com>
2016-02-18 15:29:04 -0800

    Setup the app.js router

---
+++ client/app/app.js
@@ -0,0 +1,18 @@
+angular.module('hunt', [])
+
+
+.config(['$routeProvider', '$locationProvider', '$httpProvider', 
+  function ($routerProvider, $locationProvider) {
+    $routerProvider
+    .when('/signin', {
+      templateUrl: 'users/signin.html'
+    })
+    .when('/main', {
+      templateUrl: 'home/main.html',
+      controller: 'UserController'
+    })
+    .otherwise({
+      redirectTo: '/signin'
+    });
+  }
+]);
