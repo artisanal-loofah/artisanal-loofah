f2e0ebde0689b6597528c5b9643aaa07d0cbf363
Albert Huynh <alberthuynh91@gmail.com>
2016-02-18 15:42:31 -0800

    Completed creating backlog schema

--- server/db.js
+++ server/db.js
@@ -3,3 +3,15 @@ var Sequelize = require('sequelize');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/hunt';
 
 var sequelize = new Sequelize(connectionString);
+
+var Backlog = db.define('Backlog', {
+  id: Sequelize.INTEGER,
+  application_id: Sequelize.INTEGER,
+  notes: Sequelize.TEXT,
+  status: Sequelize.String
+});
+
+Backlog.belongsTo(Application, {
+  foreignKey: 'application_id',
+  constraints: false
+});
