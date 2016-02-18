3ef9b254681bcdd2e88544658fab01cad99ff572
Gar Lee <gardarr@gmail.com>
2016-02-18 15:19:16 -0800

    Setup the signin/up with Linkedin operation, not yet functional

---
+++ client/app/users/UserController.js
@@ -0,0 +1,30 @@
+angular.module('hunt.users', [])
+
+.controller('UserController', function ($scope, $window, $location, UserFactory) {
+
+  $scope.linkedinMsg = {};
+  $scope.showLinkedinLogin = true;
+  $scope.showEmailForm = true;
+
+  $scope.signIn = function (data) {
+    console.log('profile data from cb: ', data);
+  };
+
+  $scope.signOut = function () {
+    UserFactory.signOut();
+  };
+
+})
+
+.factory('UserFactory', function ($http) {
+
+  var signIn = function () {
+    $http({
+      method = 'POST',
+
+    })
+  }
+
+
+
+})
