1858eb9cbb58ae841306f48bc1d901a24ee186e9
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-18 16:02:47 -0800

    Merge backlog schema changes

--- server/db.js
+++ server/db.js
@@ -4,13 +4,50 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/hu
 
 var sequelize = new Sequelize(connectionString);
 
+// Most fields populated from LinkedIn API
+var User = db.define('users', {
+	first_name: Sequelize.STRING,
+	last_name: Sequelize.STRING,
+	email: Sequelize.STRING,
+	headline: Sequelize.STRING, // title and company from LinkedIn API
+	linkedin_id: Sequelize.STRING,
+	picture_url: Sequelize.STRING
+});
+
+var Company = db.define('companies', {
+	name: Sequelize.STRING
+});
+
+var Application = db.define('applications', {
+	user_id: Sequelize.INTEGER,
+	company_id: Sequelize.INTEGER,
+  	job_title: Sequelize.STRING
+});
+
 var Backlog = db.define('Backlog', {
   application_id: Sequelize.INTEGER,
   notes: Sequelize.TEXT,
   status: Sequelize.String
 });
 
+User.hasMany(Application, {foreignKey: 'user_id'});
+Application.belongsTo(User, {foreignKey: 'user_id'});
+
+Company.hasMany(Application, {foreignKey: 'company_id'});
+Application.belongsTo(Company, {foreignKey: 'company_id'});
+
 Backlog.belongsTo(Application, {
   foreignKey: 'application_id',
   constraints: false
 });
+Application.hasMany(Backlog, {foreignKey: 'application_id'});
+
+User.sync();
+Company.sync();
+Application.sync();
+Backlog.sync();
+
+module.exports.User = User;
+module.exports.Company = Company;
+module.exports.Application = Application;
+module.exports.Backlog = Backlog;
