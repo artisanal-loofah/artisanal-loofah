dd52f3919fd4b61a1476ead58a718ee8a0bf7aaa
Gar Lee <gardarr@gmail.com>
2016-02-19 15:59:57 -0800

    Completed clientside appsubmit structure HTML/Factory/Controller

--- client/app/dashboard/appSubmit.js
+++ client/app/dashboard/appSubmit.js
@@ -1,5 +1,78 @@
-angular.module('hunt.appSubmit', [])
+angular.module('hunt.appSubmit', ['hunt.backlog'])
 
-.controller('AppSubmitController', function ($scope) {
+.controller('AppSubmitController', function ($scope, AppSubmitFactory) {
+  $scope.appSubmitInfo = {};
+  $scope.appSubmitList = [];
+
+  $scope.getAppSubmits = function () {
+    AppSubmitFactory.findAll();
+  };
+
+  $scope.modify = function (target) {
+    AppSubmitFactory.edit(target);
+  };
+
+  $scope.removeApp = function (target) {
+    AppSubmitFactory.removeApp(target);
+  };
+
+
+  $scope.getAppSubmits();
+})
+
+.factory('AppSubmitFactory', function ($http) {
+
+  var findAll = function (callback) {
+    return $http({
+      method: 'GET',
+      url: '/api/appsubmits'
+    }).then(function (response) {
+      if (callback) {
+        return callback(response.data);
+      } else {
+        return response.data;
+      }
+    })
+  };
+
+  var addNew = function (data) {
+    return $http({
+      method: 'POST',
+      url: 'api/appsubmits',
+      data: data
+    }).then(function (response) {
+      response.status = 201
+      return response;
+    });
+  };
+
+  var removeApp = function (data) {
+    return $http({
+      method: 'POST',
+      url: 'api/appsubmits',
+      data: data
+    }).then(function (response) {
+      response.status = 201
+      return response;
+    })
+  };
+
+  var edit = function (data) {
+    return $http({
+      method: 'POST',
+      url: 'api/appsubmits',
+      data: data
+    }).then(function (response) {
+      response.status = 201
+      return response;
+    })
+  }
+
+  return {
+    findAll: findAll,
+    addNew: addNew,
+    remove: remove,
+    edit: edit
+  }
 
 });
