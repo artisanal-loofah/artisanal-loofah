9f8e74d367c85dc7048456d677b1a93443003cf3
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-18 20:58:48 -0800

    Add user controller with node server, moved app.js to main folder

--- client/app/app.js
+++ client/app/app.js
@@ -1,18 +1,18 @@
 angular.module('huntApp', [
-  'hunt.users',
-  'ngRoute'])
+ 'hunt.users',
+ 'ngRoute'])
 
 .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-    .when('/signin', {
-      templateUrl: 'app/users/signin.html'
-    })
-    .when('/main', {
-      templateUrl: 'app/home/main.html',
-      controller: 'UserController'
-    })
-    .otherwise({
-      redirectTo: '/main'
-    });
-  }
+   $routeProvider
+   .when('/signin', {
+     templateUrl: 'app/users/signin.html'
+   })
+   .when('/main', {
+     templateUrl: 'app/home/main.html',
+     controller: 'UserController'
+   })
+   .otherwise({
+     redirectTo: '/signin'
+   });
+ }
 );
--- client/app/users/UserController.js
+++ client/app/users/UserController.js
@@ -1,25 +1,28 @@
 angular.module('hunt.users', [])
 
 .controller('UserController', function huntUsers($scope, $location, $rootScope, $http) {
-  $scope.user = {};
+ 
+ $scope.getLinkedInData = function () {
+   if(!$scope.hasOwnProperty("userprofile")){
+     IN.API.Profile("me").fields(
+         [ "id", "firstName", "lastName", "pictureUrl", "publicProfileUrl" ])
+       .result(function(result) {
+         console.log(result)
+       $rootScope.$apply(function() {
+         $rootScope.userprofile = result.values[0];
+         $rootScope.loggedUser = true;
+         $location.path("/main");
+       });
+     }).error(function(err) {
+       $scope.error = err;
+     });
+   }
+ };
 
-  $scope.getLinkedInData = function () {
-    if (!Object.keys($scope.user).length) {
-      IN.API.Profile("me").fields(
-          [ "id", "firstName", "lastName", "pictureUrl", "publicProfileUrl" ])
-        .result(function(response) {
-          console.log(response);
-          $scope.user = response.values[0];
-          $location.path("/main");
-      }).error(function(err) {
-        $scope.error = err;
-      });
-    }
-  };
-
-  $scope.logoutLinkedIn = function () {
-    IN.User.logout();
-    $scope.user = {};
-    $location.path('/signin');
-  }
+ $scope.logoutLinkedIn = function () {
+   IN.User.logout();
+   delete $rootScope.userprofile;
+   $rootScope.loggedUser = false;
+   $location.path('/signin');
+ }
 })
