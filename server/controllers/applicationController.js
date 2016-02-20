17aefd45652c49971c507b60427ae6a372633156
chououtside <chouming3@gmail.com>
2016-02-19 19:54:20 -0800

    add comments to the application controller file, documenting functionality

--- server/controllers/applicationController.js
+++ server/controllers/applicationController.js
@@ -1,7 +1,6 @@
 var Application = require('../models/application');
 var Company = require('../models/company');
 
-
 module.exports = {
   createApp: function(req, res) {
     console.log(req.body.company);
@@ -11,43 +10,34 @@ module.exports = {
       job_title: req.body.jobTitle
     };
 
+    //Must checks the Company table to see if the company already exists
     Company.get(companyName, function(company) {
+      //If company exists create a new entry in the Company table
       if (!company) {
         var newCompany = {
           name: companyName,
         };
 
         Company.post(newCompany, function(companyId) {
+          //adding company_id property must be added to create an application, but is dependent on the Company query
           applicationData.company_id = companyId;
 
           Application.post(applicationData, function(application) {
             res.statusCode = 201;
             res.json(application);
           });
-
         });
+
       } else {
+        //Company.get returns the row from the query which contains the ID that must be added onto the applicationData object
+        //to create a new application in the databse
         applicationData.company_id = company.id;
         Application.post(applicationData, function(application) {
           res.statusCode = 201;
           res.json(application);
         });
-
-
       }
     });
-
-
-    console.log(req.body)
-
-  },
-  post: function(req, res) {
-    var userData = req.body;
-    console.log(req.body);
-    res.send({ data: 'hi' });
-
   }
 };
 
-
-// userId: 1, jobTitle: 'SW', company: 'Twitter'
