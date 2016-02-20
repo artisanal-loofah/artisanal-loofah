59c57a95f68d395c0fd6089974d488d6b3fd8b35
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-19 19:12:10 -0800

    Add html template for onSite list items, stub angular controller for getOnSites

--- client/app/dashboard/onSite.js
+++ client/app/dashboard/onSite.js
@@ -1,5 +1,18 @@
 angular.module('hunt.onSite', [])
 
-.controller('OnSiteController', function ($scope) {
+.controller('OnSiteController', function ($scope, OnSiteFactory) {
+  $scope.onSiteList = [];
+
+  $scope.getOnSites = function () {
+    OnSiteFactory.findAll()
+    .then(function (onSites) {
+      $scope.onSiteList = onSites;
+    }).catch(function (error) {
+      console.error(error);
+    });
+  };
+})
+
+.factory('OnSiteFactory', function ($http) {
 
 });
