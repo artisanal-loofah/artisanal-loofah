705fb34fa5795ac6b263d3b9465c2c3eee0387e1
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-19 19:50:30 -0800

    Add routing to appSubmit and onSite controllers

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,8 @@
 var router = require('express').Router();
 var userController = require('./controllers/userController');
 var backlogController = require('./controllers/backlogController');
+var appSubmitController = require('./controllers/appsubmitController');
+var onSiteController = require('./controllers/onSiteController');
 
 //Connect controller methods to their corresponding routes
 router.get('/api/users', userController.get);
@@ -9,5 +11,16 @@ router.post('/api/users', userController.post);
 router.get('/api/backlogs', backlogController.allBacklogs);
 router.post('/api/backlogs', backlogController.newBacklog);
 router.put('/api/backlogs', backlogController.updateBacklog);
+
+// List Item controllers
+// Note: GET requests return ALL list items
+
+router.get('/api/appsubmits', appSubmitController.getAllApps);
+router.post('/api/appsubmits', appSubmitController.addApp);
+router.put('/api/appsubmits', appSubmitController.editApp);
+
+router.get('/api/onsites', onSiteController.getAllOnSites);
+router.post('/api/onsites', onSiteController.addOnSite);
+router.put('/api/onsites', onSiteController.editOnSite);
 
 module.exports = router;
