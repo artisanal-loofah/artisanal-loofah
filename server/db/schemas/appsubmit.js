f6d18822d243e595375702289368ab92a286a8bc
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-22 15:25:14 -0800

    Add user_id as foreign key

--- server/db/schemas/appsubmit.js
+++ server/db/schemas/appsubmit.js
@@ -2,6 +2,7 @@ var db = require('../db');
 var Sequelize = require('sequelize');
 
 var AppSubmit = db.define('AppSubmit', {
+  user_id: Sequelize.INTEGER,
   application_id: Sequelize.INTEGER,
   notes: Sequelize.TEXT,
   status: Sequelize.STRING
--- server/db/schemas/backlog.js
+++ server/db/schemas/backlog.js
@@ -2,6 +2,7 @@ var db = require('../db');
 var Sequelize = require('sequelize');
 
 var Backlog = db.define('Backlog', {
+  user_id: Sequelize.INTEGER,
   application_id: Sequelize.INTEGER,
   notes: Sequelize.TEXT,
   status: Sequelize.STRING
