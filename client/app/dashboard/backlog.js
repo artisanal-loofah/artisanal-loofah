5c056eb17c678bbf5aaa699c73f2a666e07efb71
Gar Lee <gardarr@gmail.com>
2016-02-23 13:29:14 -0800

    Modularized backlog factory, cleaned services.js

--- client/app/dashboard/backlog.js
+++ client/app/dashboard/backlog.js
@@ -63,4 +63,53 @@ angular.module('hunt.backlog', [])
   };
 
   initializeBacklogs();
-});
+})
+
+.factory('Backlog', function ($http) {
+
+  // Retrieves all backlogs stored in database
+  var getBacklogs = function (userId) {
+    return $http({
+      method: 'GET',
+      url: '/api/backlogs',
+      params: {
+        userId: userId
+      }
+    })
+    .then(function (resp) {
+      console.log('GET request to /api/backlogs/ successful! The response is: ', resp.data);
+      return resp.data;
+    });
+  };
+
+  var submitBacklogChanges = function (backlog) {
+    console.log('clientside backlog: ', backlog);
+    return $http({
+      method: 'PUT',
+      url: '/api/backlogs',
+      data: backlog
+    })
+    .then(function (resp) {
+      console.log('PUT request to /api/backlogs successful! The response is: ', resp);
+      return resp.data;
+    });
+  };
+
+  var addBacklog = function (backlog) {
+    return $http({
+      method: 'POST',
+      url: '/api/backlogs',
+      data: backlog
+    })
+    .then(function (resp) {
+      console.log('POST request to /api/backlogs successful! The response is: ', resp);
+      return resp;
+    });
+  };
+
+  return {
+    getBacklogs: getBacklogs,
+    submitBacklogChanges: submitBacklogChanges,
+    addBacklog: addBacklog
+  }
+})
