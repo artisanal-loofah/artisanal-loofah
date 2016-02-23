fc4953745f73558d07e5c617be03d9c19ba60d41
Gar Lee <gardarr@gmail.com>
2016-02-23 12:55:53 -0800

    Setup clientside phonescreen create/add/edit functionality

--- client/app/dashboard/phoneScreen.js
+++ client/app/dashboard/phoneScreen.js
@@ -1,10 +1,70 @@
 angular.module('hunt.phoneScreen', [])
 
-.controller('PhoneScreenController', function ($scope, phoneScreenFactory) {
+.controller('PhoneScreenController', function ($scope, PhoneScreenFactory) {
+  $scope.phoneScreenList = [];
 
+  $scope.getPhoneScreens = function () {
+    PhoneScreenFactory.findAll($rootScope.user.id)
+    .then(function (phoneScreenList) {
+      $scope.phoneScreenList = phoneScreenList;
+    }).catch(function (error) {
+      console.error(error);
+    });
+  };
 
+  $scope.modify = function (phoneScreenListItem) {
+    PhoneScreenFactory.edit(phoneScreenListItem);
+  };
+
+  $scope.removePhoneScreen = function (phoneScreenListItem) {
+    PhoneScreenFactory.edit(phoneScreenListItem);
+  };
+
+  $scope.getPhoneScreens();
 })
 
-.factory('phoneScreenFactory', function ($http) {
+.factory('PhoneScreenFactory', function ($http) {
+  var findAll = function (userId) {
+    return $http({
+      method: 'GET',
+      url: 'api/phonescreens',
+      params: {
+        userId: userId
+      }
+    }).then(function (response) {
+      return response.data;
+    }).catch(function (error) {
+      console.error(error);
+    })
+  };
 
+  var addNew = function (phoneScreenListItem) {
+    return $http({
+      method: 'POST',
+      url: 'api/phonescreens',
+      data: phoneScreenListItem
+    }).then(function (response) {
+      return response.data;
+    }).catch(function (error) {
+      console.error(error);
+    });
+  };
+
+  var edit = function (phoneScreenListItem) {
+    return $http({
+      method: 'PUT',
+      url: 'api/phonescreens',
+      data: phoneScreenListItem
+    }).then(function (response) {
+      return response.data;
+    }).catch(function (error) {
+      console.error(error);
+    });
+  };
+
+  return {
+    findAll: findAll,
+    addNew: addNew,
+    edit: edit
+  }
 });
