b551f250b9fb23bad3608d47ddfd6e31dcdbe090
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-23 15:54:46 -0800

    Rewrite getLinkedInData

--- client/app/users/UserController.js
+++ client/app/users/UserController.js
@@ -1,55 +1,54 @@
 angular.module('hunt.users', [])
 
 .controller('UserController', function huntUsers($scope, $location, $rootScope, $http, $window, User) {
- 
-
-  $scope.getLinkedInData = function () {
-    // $scope.userData = {};
-    // $scope.user = {};
-    console.log('getting linkedin data, check rootscope');
-    console.log($rootScope);
-
-    if(!$scope.hasOwnProperty("userprofile")){
-      IN.API.Profile("me").fields([ "id", "firstName", "lastName", "pictureUrl", "publicProfileUrl", "headline" ])
-      .result(function(result) {
-        $window.localStorage.setItem('com.token', null);
-        $rootScope.$apply(function() {
-
-          $rootScope.userprofile = result.values[0];
-          // $scope.userData = result.values[0];
-          $rootScope.loggedUser = true;
-
-          User.getUser($rootScope.userprofile.id)
-          .then(function(res) {
-            // test if user is empty object
-            if (res.data) {
-              $rootScope.user = res.data;
-            } else {
-              $rootScope.user = User.createUser($rootScope.userprofile);
-            }
-            $rootScope.initializeBacklogs();
-            $rootScope.initializeAppSubmits();
-            // $rootScope.initializePhoneScreens();
-            // $rootScope.initializeOnSites();
-            // $rootScope.initializeOffers();
-            $location.path("/main");
-          })
-          .catch(function(err) {
-            console.error(err);
-          })
-
-        });
-      }).error(function(err) {
-        $scope.error = err;
+  $scope.initializeApp = function() {
+    if (!isLoggedIn()) {
+      $scope.getLinkedInData(function() {
+        $location.path("/main");
+      });
+    } else {
+      var id = $window.localStorage.getItem('user_id');
+      User.getUserById(id)
+      .then(function(user) {
+        $rootScope.user = user;
+        $location.path("/main");
       });
     }
   };
 
+  $scope.getLinkedInData = function (callback) {
+    IN.API.Profile("me").fields([ "id", "firstName", "lastName", "pictureUrl", "publicProfileUrl", "headline" ])
+    .result(function(result) {
+      $rootScope.$apply(function() {
+        $rootScope.userprofile = result.values[0];
+        $rootScope.loggedUser = true;
+
+        User.getUserByLinkedInId($rootScope.userprofile.id)
+        .then(function(user) {
+          // test if user is empty object
+          if (user) {
+            $rootScope.user = user;
+          } else {
+            $rootScope.user = User.createUser($rootScope.userprofile);
+          }
+          $window.localStorage.setItem('user_id', $rootScope.user.id);
+          callback();
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+      });
+    }).error(function(err) {
+      $scope.error = err;
+    });
+  };
+
   $scope.logoutLinkedIn = function () {
+    IN.User.logout();
     delete $rootScope.userprofile;
     delete $rootScope.user;
     $rootScope.loggedUser = false;
-    $window.localStorage.removeItem('com.token');
+    $window.localStorage.removeItem('user_id');
     $location.path('/signin');
   };
 });
