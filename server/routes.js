f3642309af8a81f641ae2bdbfc98998864fb05ee
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-26 18:15:11 -0800

    Add offer routes

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,7 @@ var appSubmitController = require('./controllers/appsubmitController');
 var phoneScreenController = require('./controllers/phoneScreenController');
 var onSiteController = require('./controllers/onSiteController');
 var applicationController = require('./controllers/applicationController');
+var offerController = require('./controllers/onSiteController');
 
 //Connect controller methods to their corresponding routes
 router.get('/api/users', userController.get);
@@ -32,5 +33,9 @@ router.put('/api/phonescreens', phoneScreenController.updatePhoneScreen);
 router.get('/api/onsites', onSiteController.allOnSites);
 router.post('/api/onsites', onSiteController.addOnSite);
 router.put('/api/onsites', onSiteController.updateOnSite);
+
+router.get('/api/onsites', offerController.allOffers);
+router.post('/api/onsites', offerController.addOffer);
+router.put('/api/onsites', offerController.updateOffer);
 
 module.exports = router;
