efa98f2d2b1218b65cfcb94fdb43776635df8bb9
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-28 13:10:55 -0800

    Requirements for user controller test

---
+++ spec/controllers/userControllerTest.js
@@ -0,0 +1,5 @@
+var expect = require('chai').expect;
+var User = require('../server/models/user');
+var userController = require('../server/controllers/userController');
+
+var Sequelize = require('sequelize');
