296c3d88c9abfcd17ea921803327e3c751bc4776
chououtside <chouming3@gmail.com>
2016-02-28 14:38:26 -0800

    remove old comments

--- app.js
+++ app.js
@@ -25,11 +25,6 @@ app.use(parser.json());
 var router = require('./server/routes');
 app.use(router);
 
-// Logging and parsing
-
-// Set up our routes
-// app.use('/classes', router);
-
 // Serve the client files
 app.use(express.static(__dirname + '/client'));
 
