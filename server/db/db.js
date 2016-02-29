20e8a267c02c57fdc453948686e9aca8575e5bf5
Akshay Buddiga <abuddiga@stanford.edu>
2016-02-29 12:13:25 -0800

    Vary db location based on process.env.TEST variable

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,11 @@
 var pg = require('pg');
 var Sequelize = require('sequelize');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/hunt';
+var connectionString;
+
+if (process.env.TEST) {
+  connectionString = 'postgres://localhost:5432/hunttest';
+} else {
+  connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/hunt';
+}
 
 module.exports = new Sequelize(connectionString);
