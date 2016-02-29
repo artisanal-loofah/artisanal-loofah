2ac09637518eea0055453d0ff8b1d2f94cea6885
Gar Lee <gardarr@gmail.com>
2016-02-29 10:47:44 -0800

    cleaning more

--- client/app/dashboard/application.js
+++ client/app/dashboard/application.js
@@ -2,7 +2,7 @@ angular.module('hunt.application', ['hunt.users'])
 
 .controller('ApplicationController', function ($scope, $rootScope, Application, Backlog) {
 
-  $scope.addApplication = function (){
+  $scope.addApplication = function () {
     // only create application if job title and company name are not empty or whitespace
     if ($scope.jobTitle.trim().length && $scope.company.trim().length) {
       // user id is added on the backend
@@ -22,6 +22,9 @@ angular.module('hunt.application', ['hunt.users'])
         });
       });
     }
+    console.log('stuff: ', $scope.jobTitle, $scope.company)
+  $scope.jobTitle = '';
+  $scope.company = '';
   };
 })
 
