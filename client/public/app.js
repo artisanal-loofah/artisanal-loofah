9bf60bd7a6cad9f02c7b51f8b319fd61b92036e2
Gar Lee <gardarr@gmail.com>
2016-02-29 16:37:50 -0800

    Enlarged size of linkedin login button

--- client/public/app.js
+++ client/public/app.js
@@ -836,6 +836,8 @@ angular.module('hunt.users', [])
 });
 
 function onLinkedInLoad() {
+  $('a[id*=li_ui_li_gen_]').css({marginBottom:'20px'}) 
+  .html('<img src="../assets/Sign-In-Large---Default.png" height="50" width="250" border="0" />');
   console.log('loading linkedin');
   IN.Event.on(IN, "auth", function() {
     onLinkedInLogin();
